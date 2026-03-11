d31e8275354fa898d7a07d95492f0dd85e52a150
Mena599 <20243rd010@utez.edu.mx>
2026-03-11 00:30:44 -0600

    Componentes de Usuario echos y vistas de empleado empesadas

--- mobile-andoid/Activos360/app/build.gradle.kts
+++ mobile-andoid/Activos360/app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("org.openapi.generator") version "7.20.0"
 }
 
 android {
@@ -40,26 +39,6 @@ android {
     }
 }
 
-openApiGenerate {
-    generatorName.set("kotlin")
-    // 1. Ruta a tu archivo generado con IntelliJ
-    inputSpec.set("$projectDir/src/main/resources/api-docs.yaml")
-
-    // 2. Donde se guardará el código (no lo toques, déjalo en build)
-    outputDir.set("$buildDir/generated/openapi")
-
-    // 3. Nombres de tus paquetes
-    apiPackage.set("com.tuapp.network.api")
-    modelPackage.set("com.tuapp.network.model")
-
-    configOptions.set(mapOf(
-        "library" to "jvm-retrofit2", // Usa Retrofit 2
-        "serializationLibrary" to "moshi", // O "gson" / "kotlinx_serialization"
-        "useCoroutines" to "true", // Genera funciones 'suspend'
-        "omitGradleWrapper" to "true"
-    ))
-}
-
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -70,6 +49,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- mobile-andoid/Activos360/app/src/main/java/com/example/activos360/MainActivity.kt
+++ mobile-andoid/Activos360/app/src/main/java/com/example/activos360/MainActivity.kt
@@ -4,16 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.activos360.ui.ScreensLogin.LoginScreen
-import com.example.activos360.ui.ScreensLogin.ScreeanCreatePassword
-import com.example.activos360.ui.screens_login.LoginInitialScreen
+import com.example.activos360.ui.Screens.Login.LoginScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ScreeanCreatePassword()
+            LoginScreen()
         }
     }
 }
